78d476e9afdcac99055608859701c8538d10b47c
OleksandrTEB <fiuhad@gmail.com>
2025-10-21 14:45:32 +0200

    Update

--- script.js
+++ script.js
@@ -10,48 +10,17 @@
 //     }
 // }
 
-function characterDescription(mojaPostac) {
-    console.log("=== KARTA POSTACI ===")
-    console.log(`Imie: ${mojaPostac.name}`)
-    console.log(`Klasa: ${mojaPostac.role}`)
-    console.log(`Poziom: ${mojaPostac.level}`)
 
-    console.log('')
-
-    console.log("Umiejętności:")
-    mojaPostac.skills.forEach((skill) => {
-        console.log(`- ${skill}`)
-    })
-
-
-    console.log('')
-
-    console.log("Ekwipunek:")
-    console.log(`Broń: ${mojaPostac.equipment.items.armor}`)
-    console.log(`Zbroja: ${mojaPostac.equipment.items.weapon}`)
-
-    console.log(`Przedmioty:`)
-    mojaPostac.equipment.items.forEach((item) => {
-        console.log(`- ${item}`)
-    })
-
-    console.log('')
-}
-function levelUp(mojaPostac) {
-    mojaPostac.level++
-    characterDescription(mojaPostac)
-}
-function learnSkill(mojaPostac, newSkill) {
-    if (!newSkill) {
-        alert("Wprowadż poprawny skill")
-        return;
+const mojaPostacFake = {
+    level: 0,
+    skills: [],
+    equipment: {
+        weapon: "",
+        armor: "",
+        items: []
     }
-    if (mojaPostac .skills.length === 5) {
-        alert("Nie może mieć więcej niż 5 umiejętności")
-        return
-    }
-    mojaPostac .skills.push(newSkill)
 }
+
 function createCharacter(name, role) {
     if (name.length === 3) {
         alert("Musi mieć minimum 3 znaki")
@@ -72,41 +41,65 @@ function wearArmor(mojaPostac, armor) {
     mojaPostac.equipment.items.armor = armor
 }
 
-function wearWeapon(mojaPostac, armor) {
-    mojaPostac.equipment.items.armor = armor
+function wearWeapon(mojaPostac, weapon) {
+    mojaPostac.equipment.items.weapon = weapon
 }
 
 
-const mojaPostac = {
-    name: "",
-    role: "",
-    level: 0,
-    skills: [],
-    equipment: {
-        weapon: "",
-        armor: "",
-        items: []
+
+
+function characterDescription(mojaPostac) {
+    console.log("=== KARTA POSTACI ===")
+    console.log(`Imie: ${mojaPostac.name}`)
+    console.log(`Klasa: ${mojaPostac.role}`)
+    console.log('Poziom: ', +mojaPostac.level)
+
+    console.log('')
+
+    console.log("Umiejętności:")
+    mojaPostac.skills.forEach((skill) => {
+        console.log(`- ${skill}`)
+    })
+
+    console.log('')
+
+    console.log("Ekwipunek:")
+    console.log(`Broń: ${mojaPostac.equipment.items.armor}`)
+    console.log(`Zbroja: ${mojaPostac.equipment.items.weapon}`)
+
+    console.log(`Przedmioty:`)
+    mojaPostac.equipment.items.forEach((item) => {
+        console.log(`- ${item}`)
+    })
+
+    console.log('')
+}
+function levelUp(mojaPostac) {
+    mojaPostac.level++
+    characterDescription(mojaPostac)
+}
+
+function learnSkill(mojaPostac, newSkill) {
+    if (!newSkill) {
+        alert("Wprowadż poprawny skill")
+        return;
     }
+    if (mojaPostac .skills.length === 5) {
+        alert("Nie może mieć więcej niż 5 umiejętności")
+        return
+    }
+    mojaPostac .skills.push(newSkill)
 }
 
-createCharacter("Adam", "pomocniczy")
-
-addItem(mojaPostac, "Kompas", "Kostur", "Szata")
+const mojaPostac = Object.assign({}, mojaPostacFake, createCharacter("Adam", "Pomocnik"))
 
 
+addItem(mojaPostac, "Kompas")
+wearArmor(mojaPostac, "Kostur")
+wearWeapon(mojaPostac, "Szata")
 
 
 
 learnSkill(mojaPostac, "Support")
-
-function addSkill() {
-    learnSkill(mojaPostac, prompt("Umiejętniść"))
-    characterDescription(mojaPostac)
-}
-
-function addItemBtn(mojaPostac) {
-    mojaPostac.equipment.items.push(prompt("Dodaj przedmiot"))
-    characterDescription(mojaPostac)
-}
 
 characterDescription(mojaPostac)
